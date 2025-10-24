451c948dafebbdf77307fb13494579ec59e69a78
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-10-24 15:50:15 -0600

    THIS DOESN'T WORK I AM TIRED JEFE

--- SerialComExample/SerialComExample.Designer.vb
+++ SerialComExample/SerialComExample.Designer.vb
@@ -27,6 +27,10 @@ Partial Class SerialComExample
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.CountTextBox = New System.Windows.Forms.TextBox()
         Me.PWMTrackBar = New System.Windows.Forms.TrackBar()
+        Me.BytesToReadTextBox = New System.Windows.Forms.TextBox()
+        Me.HandShakeAsciiTextBox = New System.Windows.Forms.TextBox()
+        Me.HandShakeHexTextBox = New System.Windows.Forms.TextBox()
+        Me.SendDataButton = New System.Windows.Forms.Button()
         CType(Me.PWMTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,11 +39,11 @@ Partial Class SerialComExample
         '
         'Timer1
         '
-        Me.Timer1.Interval = 25
+        Me.Timer1.Interval = 1000
         '
         'CountTextBox
         '
-        Me.CountTextBox.Location = New System.Drawing.Point(500, 151)
+        Me.CountTextBox.Location = New System.Drawing.Point(195, 68)
         Me.CountTextBox.Name = "CountTextBox"
         Me.CountTextBox.ReadOnly = True
         Me.CountTextBox.Size = New System.Drawing.Size(100, 22)
@@ -47,19 +51,55 @@ Partial Class SerialComExample
         '
         'PWMTrackBar
         '
-        Me.PWMTrackBar.Location = New System.Drawing.Point(371, 151)
+        Me.PWMTrackBar.LargeChange = 1
+        Me.PWMTrackBar.Location = New System.Drawing.Point(12, 96)
         Me.PWMTrackBar.Maximum = 25
         Me.PWMTrackBar.Minimum = 5
         Me.PWMTrackBar.Name = "PWMTrackBar"
-        Me.PWMTrackBar.Size = New System.Drawing.Size(104, 56)
+        Me.PWMTrackBar.Size = New System.Drawing.Size(482, 56)
         Me.PWMTrackBar.TabIndex = 1
+        Me.PWMTrackBar.TickFrequency = 20
         Me.PWMTrackBar.Value = 5
+        '
+        'BytesToReadTextBox
+        '
+        Me.BytesToReadTextBox.Location = New System.Drawing.Point(12, 12)
+        Me.BytesToReadTextBox.Name = "BytesToReadTextBox"
+        Me.BytesToReadTextBox.Size = New System.Drawing.Size(177, 22)
+        Me.BytesToReadTextBox.TabIndex = 2
+        '
+        'HandShakeAsciiTextBox
+        '
+        Me.HandShakeAsciiTextBox.Location = New System.Drawing.Point(12, 40)
+        Me.HandShakeAsciiTextBox.Name = "HandShakeAsciiTextBox"
+        Me.HandShakeAsciiTextBox.Size = New System.Drawing.Size(177, 22)
+        Me.HandShakeAsciiTextBox.TabIndex = 3
+        '
+        'HandShakeHexTextBox
+        '
+        Me.HandShakeHexTextBox.Location = New System.Drawing.Point(12, 68)
+        Me.HandShakeHexTextBox.Name = "HandShakeHexTextBox"
+        Me.HandShakeHexTextBox.Size = New System.Drawing.Size(177, 22)
+        Me.HandShakeHexTextBox.TabIndex = 4
+        '
+        'SendDataButton
+        '
+        Me.SendDataButton.Location = New System.Drawing.Point(301, 12)
+        Me.SendDataButton.Name = "SendDataButton"
+        Me.SendDataButton.Size = New System.Drawing.Size(193, 79)
+        Me.SendDataButton.TabIndex = 5
+        Me.SendDataButton.Text = "Send Data"
+        Me.SendDataButton.UseVisualStyleBackColor = True
         '
         'SerialComExample
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SendDataButton)
+        Me.Controls.Add(Me.HandShakeHexTextBox)
+        Me.Controls.Add(Me.HandShakeAsciiTextBox)
+        Me.Controls.Add(Me.BytesToReadTextBox)
         Me.Controls.Add(Me.PWMTrackBar)
         Me.Controls.Add(Me.CountTextBox)
         Me.Name = "SerialComExample"
@@ -74,4 +114,8 @@ Partial Class SerialComExample
     Friend WithEvents Timer1 As Timer
     Friend WithEvents CountTextBox As TextBox
     Friend WithEvents PWMTrackBar As TrackBar
+    Friend WithEvents BytesToReadTextBox As TextBox
+    Friend WithEvents HandShakeAsciiTextBox As TextBox
+    Friend WithEvents HandShakeHexTextBox As TextBox
+    Friend WithEvents SendDataButton As Button
 End Class
--- SerialComExample/SerialComExample.vb
+++ SerialComExample/SerialComExample.vb
@@ -18,143 +18,181 @@ Public Class SerialComExample
     End Sub
 
     Sub PWM_Select()
-        Dim data(3) As Byte
+        Dim data(2) As Byte
+        'data(0) = &H24 ' $
+        'data(0) = &H22 ' "
 
-        For Rank = 5 To 25
-            Select Case Rank
-                Case 5
-                    data(0) = &H24 '$
-                    data(1) = &H32 'PR_PW = 50
-                    data(2) = &H23 '#
-                    data(3) = &HC3 'PR_PS = 195
-                    CountTextBox.Text = "0.5ms"
-                Case 6
-                    data(0) = &H24 '$
-                    data(1) = &H3C 'PR_PW = 60
-                    data(2) = &H23 '#
-                    data(3) = &HC2 'PR_PS = 195
-                    CountTextBox.Text = "0.6ms"
-                Case 7
-                    data(0) = &H24 '$
-                    data(1) = &H46 'PR_PW = 70
-                    data(2) = &H23 '#
-                    data(3) = &HC1 'PR_PS = 193
-                    CountTextBox.Text = "0.7ms"
-                Case 8
-                    data(0) = &H24 '$
-                    data(1) = &H50 'PR_PW = 80
-                    data(2) = &H23 '#
-                    data(3) = &HC0 'PR_PS = 192
-                    CountTextBox.Text = "0.8ms"
-                Case 9
-                    data(0) = &H24 '$
-                    data(1) = &H5A 'PR_PW = 90
-                    data(2) = &H23 '#
-                    data(3) = &HBF 'PR_PS = 191
-                    CountTextBox.Text = "0.9ms"
-                Case 10
-                    data(0) = &H24 '$
-                    data(1) = &H64 'PR_PW = 100
-                    data(2) = &H23 '#
-                    data(3) = &HBE 'PR_PS = 190
-                    CountTextBox.Text = "1.0ms"
-                Case 11
-                    data(0) = &H24 '$
-                    data(1) = &H6E 'PR_PW = 110
-                    data(2) = &H23 '#
-                    data(3) = &HBD 'PR_PS = 189
-                    CountTextBox.Text = "1.1ms"
-                Case 12
-                    data(0) = &H24 '$
-                    data(1) = &H78 'PR_PW = 120
-                    data(2) = &H23 '#
-                    data(3) = &HBC 'PR_PS = 188
-                    CountTextBox.Text = "1.2ms"
-                Case 13
-                    data(0) = &H24 '$
-                    data(1) = &H82 'PR_PW = 130
-                    data(2) = &H23 '#
-                    data(3) = &HBB 'PR_PS = 187
-                    CountTextBox.Text = "1.3ms"
-                Case 14
-                    data(0) = &H24 '$
-                    data(1) = &H8C 'PR_PW = 140
-                    data(2) = &H23 '#
-                    data(3) = &HBA 'PR_PS = 186
-                    CountTextBox.Text = "1.4ms"
-                Case 15
-                    data(0) = &H24 '$
-                    data(1) = &H96 'PR_PW = 150
-                    data(2) = &H23 '#
-                    data(3) = &HB9 'PR_PS = 185
-                    CountTextBox.Text = "1.5ms"
-                Case 16
-                    data(0) = &H24 '$
-                    data(1) = &HA0 'PR_PW = 160
-                    data(2) = &H23 '#
-                    data(3) = &HB8 'PR_PS = 184
-                    CountTextBox.Text = "1.6ms"
-                Case 17
-                    data(0) = &H24 '$
-                    data(1) = &HAA 'PR_PW = 170
-                    data(2) = &H23 '#
-                    data(3) = &HB7 'PR_PS = 183
-                    CountTextBox.Text = "1.7ms"
-                Case 18
-                    data(0) = &H24 '$
-                    data(1) = &HB4 'PR_PW = 180
-                    data(2) = &H23 '#
-                    data(3) = &HB6 'PR_PS = 182
-                    CountTextBox.Text = "1.8ms"
-                Case 19
-                    data(0) = &H24 '$
-                    data(1) = &HBE 'PR_PW = 190
-                    data(2) = &H23 '#
-                    data(3) = &HB5 'PR_PS = 181
-                    CountTextBox.Text = "1.9ms"
-                Case 20
-                    data(0) = &H24 '$
-                    data(1) = &HC8 'PR_PW = 200
-                    data(2) = &H23 '#
-                    data(3) = &HB4 'PR_PS = 180
-                    CountTextBox.Text = "2.0ms"
-                Case 21
-                    data(0) = &H24 '$
-                    data(1) = &HD2 'PR_PW = 210
-                    data(2) = &H23 '#
-                    data(3) = &HB3 'PR_PS = 179
-                    CountTextBox.Text = "2.1ms"
-                Case 22
-                    data(0) = &H24 '$
-                    data(1) = &HDC 'PR_PW = 220
-                    data(2) = &H23 '#
-                    data(3) = &HB2 'PR_PS = 178
-                    CountTextBox.Text = "2.2ms"
-                Case 23
-                    data(0) = &H24 '$
-                    data(1) = &HE6 'PR_PW = 230
-                    data(2) = &H23 '#
-                    data(3) = &HB1 'PR_PS = 177
-                    CountTextBox.Text = "2.3ms"
-                Case 24
-                    data(0) = &H24 '$
-                    data(1) = &HF0 'PR_PW = 240
-                    data(2) = &H23 '#
-                    data(3) = &HB0 'PR_PS = 176
-                    CountTextBox.Text = "2.4ms"
-                Case 25
-                    data(0) = &H24 '$
-                    data(1) = &HFA 'PR_PW = 250
-                    data(2) = &H23 '#
-                    data(3) = &HAF 'PR_PS = 175
-                    CountTextBox.Text = "2.5ms"
-            End Select
-        Next
-        SerialPort1.Write(data, 0, 4)
+        Select Case PWMTrackBar.Value
+            Case 5
+                data(0) = &H24 ' $
+                data(1) = &HC3 'PR_PW = 50 32
+                data(2) = &H32 'PR_PS = 195 C3
+                CountTextBox.Text = "0.5ms"
+            Case 6
+                data(0) = &H24 ' $
+                data(1) = &H3C 'PR_PW = 60
+                data(2) = &HC2 'PR_PS = 195
+                CountTextBox.Text = "0.6ms"
+            Case 7
+                data(0) = &H24 ' $
+                data(1) = &H46 'PR_PW = 70
+                data(2) = &HC1 'PR_PS = 193
+                CountTextBox.Text = "0.7ms"
+            Case 8
+                data(0) = &H24 ' $
+                data(1) = &HC8 'PR_PW = 80 50
+                data(2) = &HB4 'PR_PS = 192 C0
+                CountTextBox.Text = "0.8ms"
+            Case 9
+                data(0) = &H24 ' $
+                data(1) = &H5A 'PR_PW = 90
+                data(2) = &HBF 'PR_PS = 191
+                CountTextBox.Text = "0.9ms"
+            Case 10
+                data(0) = &H24 ' $
+                data(1) = &H64 'PR_PW = 120
+                data(2) = &HBE 'PR_PS = 188
+                CountTextBox.Text = "1.0ms"
+            Case 11
+                data(0) = &H24 ' $
+                data(1) = &H6E 'PR_PW = 110 6E
+                data(2) = &HBD 'PR_PS = 189 BD
+                CountTextBox.Text = "1.1ms"
+            Case 12
+                data(0) = &H24 ' $
+                data(1) = &H78 'PR_PW = 120
+                data(2) = &HBC 'PR_PS = 188
+                CountTextBox.Text = "1.2ms"
+            Case 13
+                data(0) = &H24 ' $
+                data(1) = &H82 'PR_PW = 130
+                data(2) = &HBB 'PR_PS = 187
+                CountTextBox.Text = "1.3ms"
+            Case 14
+                data(0) = &H24 ' $
+                data(1) = &H8C 'PR_PW = 140
+                data(2) = &HBA 'PR_PS = 186
+                CountTextBox.Text = "1.4ms"
+            Case 15
+                data(0) = &H24 ' $
+                data(1) = &H96 'PR_PW = 150
+                data(2) = &HB9 'PR_PS = 185
+                CountTextBox.Text = "1.5ms"
+            Case 16
+                data(0) = &H24 ' $
+                data(1) = &HA0 'PR_PW = 160
+                data(2) = &HB8 'PR_PS = 184
+                CountTextBox.Text = "1.6ms"
+            Case 17
+                data(0) = &H24 ' $
+                data(1) = &HAA 'PR_PW = 170
+                data(2) = &HB7 'PR_PS = 183
+                CountTextBox.Text = "1.7ms"
+            Case 18
+                data(0) = &H24 ' $
+                data(1) = &HB4 'PR_PW = 180 B4
+                data(2) = &HB6 'PR_PS = 182 B6
+                CountTextBox.Text = "1.8ms"
+            Case 19
+                data(0) = &H24 ' $
+                data(1) = &HBE 'PR_PW = 190
+                data(2) = &HB5 'PR_PS = 181
+                CountTextBox.Text = "1.9ms"
+            Case 20
+                data(0) = &H24 ' $
+                data(1) = &HC8 'PR_PW = 200
+                data(2) = &HB4 'PR_PS = 180
+                CountTextBox.Text = "2.0ms"
+            Case 21
+                data(0) = &H24 ' $
+                data(1) = &HD2 'PR_PW = 210
+                data(2) = &HB3 'PR_PS = 179
+                CountTextBox.Text = "2.1ms"
+            Case 22
+                data(0) = &H24 ' $
+                data(1) = &HDC 'PR_PW = 220
+                data(2) = &HB2 'PR_PS = 178
+                CountTextBox.Text = "2.2ms"
+            Case 23
+                data(0) = &H24 ' $
+                data(1) = &HE6 'PR_PW = 230
+                data(2) = &HB1 'PR_PS = 177
+                CountTextBox.Text = "2.3ms"
+            Case 24
+                data(0) = &H24 ' $
+                data(1) = &HF0 'PR_PW = 240
+                data(2) = &HB0 'PR_PS = 176
+                CountTextBox.Text = "2.4ms"
+            Case 25
+                data(0) = &H24 ' $
+                data(1) = &HFA 'PR_PW = 250
+                data(2) = &HAF 'PR_PS = 175
+                CountTextBox.Text = "2.5ms"
+            Case Else
+                data(0) = &H24 ' $
+                data(1) = &HFA 'PR_PW = 250
+                data(2) = &HAF 'PR_PS = 175
+                CountTextBox.Text = "2.5ms"
+        End Select
+        SerialPort1.Write(data, 0, 3)
     End Sub
 
-    Private Sub PWMTrackBar_Scroll(sender As Object, e As EventArgs) Handles PWMTrackBar.Scroll
+    'Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+    'CheckForIllegalCrossThreadCalls = False
+    'Dim numberOfBytes = SerialPort1.BytesToRead
+    'Dim' buffer(numberOfBytes - 1) As Byte
+    'Dim got As Integer = SerialPort1.Read(buffer, 0, numberOfBytes)
+
+    ' BytesToReadTextBox.Text = CStr(numberOfBytes)
+    'If got > 0 Then
+    '     AppendRX(buffer, got)
+    ' End If
+
+    'End Sub
+
+    'Private Sub AppendRX(data() As Byte, count As Integer)
+    'Dim hexLine As New System.Text.StringBuilder()
+    'Dim asciiLine As New System.Text.StringBuilder()
+
+    'F'or i = 0 To count - 1
+    'Dim b = data(i)
+    '       asciiLine.Append(If(b >= 32 AndAlso b <= 126, ChrW(b), "."c))
+    '        hexLine.Append(b.ToString("X2")).Append(" ")
+    ' Next
+    '
+    '   HandShakeAsciiTextBox.Text = asciiLine.ToString()
+    '   HandShakeHexTextBox.Text = hexLine.ToString()
+    '
+    ' If HandShakeHexTextBox.Text = "25 " Then
+    'PWM_Select()
+    'Timer1.Start()
+    ' Else
+    '        HandShakeAsciiTextBox.Text = asciiLine.ToString()
+    '       HandShakeHexTextBox.Text = hexLine.ToString()
+    'End If
+
+    'End Sub
+
+    Private Sub SerialComExample_Load(sender As Object, e As EventArgs) Handles Me.Load
         Connect()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        CheckForIllegalCrossThreadCalls = False
+        Dim numberOfBytes = SerialPort1.BytesToRead
+        If numberOfBytes = 0 Then
+            HandShakeAsciiTextBox.Text = ""
+            HandShakeHexTextBox.Text = ""
+        End If
+        Timer1.Stop()
+    End Sub
+
+    Private Sub SendDataButton_Click(sender As Object, e As EventArgs) Handles SendDataButton.Click
+        'Timer1.Start()
         PWM_Select()
+
+        'Dim data(0) As Byte
+        'Data(0) = &H24 '$
+        'SerialPort1.Write(data, 0, 1)
     End Sub
 End Class
